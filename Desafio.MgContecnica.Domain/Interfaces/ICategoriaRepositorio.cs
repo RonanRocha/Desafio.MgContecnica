a33fda240c2703ab8de3088dbb1a0ec935af4b82
Ronan Rocha <ronan.rochasouza@gmail.com>
2025-08-14 20:46:23 -0300

    feat: inclusao de absatracao basica de crud de transacao e categoria

---
+++ Desafio.MgContecnica.Domain/Interfaces/ICategoriaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Desafio.MgContecnica.Domain.Entities;
+
+namespace Desafio.MgContecnica.Domain.Interfaces
+{
+    public interface ICategoriaRepositorio
+    {
+        Task<IEnumerable<Categoria>> RecuperarTodasAsync();
+        Task<Categoria> RecuperarPorIdAsync(int id);
+        Task<Categoria> CriarCategoriaAsync(Categoria categoria);
+        Task<Categoria> AtualizarCategoriaAsync(Categoria categoria);
+        Task<Categoria> RemoverCategoriaAsync(Categoria categoria);
+    }
+}
---
+++ Desafio.MgContecnica.Domain/Interfaces/ITransacaoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Desafio.MgContecnica.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.MgContecnica.Domain.Interfaces
+{
+    public interface ITransacaoRepositorio
+    {
+        Task<IEnumerable<Transacao>> RecuperarTodasTransacoesAsync();
+        Task<Transacao> RecuperarTransacaoPorIdAsync(int id);
+        Task<Transacao> CriarTransacaoAsync(Transacao transacao);
+        Task<Transacao> AtualizarTransacaoAsync(Transacao transacao);
+        Task<Transacao> RemoverTransacaoAsync(Transacao transacao);
+    }
+}
