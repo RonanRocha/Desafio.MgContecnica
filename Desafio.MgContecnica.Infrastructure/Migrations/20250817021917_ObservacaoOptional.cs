f7fc0ebbf5839ba1cf5c34524d1f38b3f4c1d0da
Ronan Rocha <ronan.rochasouza@gmail.com>
2025-08-16 23:20:11 -0300

    feat: alteracao campo observacoes para tipo opcional no banco de dados

---
+++ Desafio.MgContecnica.Infrastructure/Migrations/20250817021917_ObservacaoOptional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Desafio.MgContecnica.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ObservacaoOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Observacoes",
+                table: "Transacoes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Observacoes",
+                table: "Transacoes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
