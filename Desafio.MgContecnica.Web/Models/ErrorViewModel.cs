515f1e4479216bbd7387fefe76d5663199908802
Ronan Rocha <ronan.rochasouza@gmail.com>
2025-08-17 12:37:59 -0300

    feat: adicionado projeto front-end

---
+++ Desafio.MgContecnica.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio.MgContecnica.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
